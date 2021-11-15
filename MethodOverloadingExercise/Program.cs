b6696d63b2306694ac3b184db22f0938b926fecb
farrah24 <msfarrahd@gmail.com>
2021-11-14 20:33:16 -0600

    initial commit

---
+++ MethodOverloadingExercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MethodOverloadingExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public int Add(int p1, int p2)
+        {
+            return p1 + p2;
+        }
+        public decimal Add (decimal d1, decimal d2)
+        {
+            return d1 + d2;
+        }
+        public string Add( int i1, int i2, bool b)
+        {
+            int sum = i1 + i2;
+            if (b)
+            {
+                if (sum == 1)
+                {
+                    return sum.ToString() + "dollar";
+                }
+                else
+                {
+                    return sum.ToString() + "dollars";
+                }
+            }
+            else
+            {
+                return sum.ToString();
+                
+            }
+        }
+    }
+}
